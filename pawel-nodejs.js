9282527bfb4dda8faee71227bfb8dfbce7d0d529
Janczarek Paweł <PJanczarek@corpo.t-mobile.pl>
2019-05-06 12:27:09 +0200

    Update pawel-nodejs.js

--- pawel-nodejs.js
+++ pawel-nodejs.js
@@ -6,7 +6,7 @@ hostname = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('<html><body><b>Hello World</b></body></html>\n');
+  res.end('<html><head><title>Paweł Janczarek - sample program</title></head><body><b>Hello World</b></body></html>\n');
 });
 
 server.listen(port, hostname, () => {
